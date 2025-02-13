bd0f37fdefeb74dc7a6801e511d9f0541def8996
Menhythien <dinhthienmenh1505@gmail.com>
2025-02-13 18:08:22 +0700

    manual change wallpaper

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ local os = require("os")
 
 local wallpaper_dir = "/DATA/pictures/wallpaper/"
 local last_update_time = os.time() -- Latest time save variable
+local current_wallpaper = nil -- current_wallpaper variable
 
 -- Get file in folder
 local function get_wallpapers()
@@ -25,7 +26,8 @@ local function get_random_wallpaper()
 	local wallpapers = get_wallpapers()
 	if #wallpapers > 0 then
 		math.randomseed(os.time()) -- Init random number
-		return wallpapers[math.random(#wallpapers)]
+		local new_wallpaper = wallpapers[math.random(#wallpapers)]
+		return new_wallpaper
 	end
 	return nil
 end
@@ -33,17 +35,29 @@ end
 -- Auto replace wallpaper after time
 wezterm.on("update-right-status", function(window, pane)
 	local now = os.time()
-	-- Kiểm tra nếu đã qua 600 giây (10 phút)
+	-- Check time 600 seconds (10 minues)
 	if now - last_update_time >= 600 then
 		local new_wallpaper = get_random_wallpaper()
-		if new_wallpaper then
+		if new_wallpaper and new_wallpaper ~= current_wallpaper then
+			current_wallpaper = new_wallpaper
 			window:set_config_overrides({
-				window_background_image = new_wallpaper,
+				window_background_image = current_wallpaper,
 			})
-			last_update_time = now -- Cập nhật lại thời gian đổi nền
+			last_update_time = now -- Update replace wallpaper time
 		end
 	end
 end)
+
+-- Function to manually change wallpaper
+local function change_wallpaper(window, pane)
+	local new_wallpaper = get_random_wallpaper()
+	if new_wallpaper and new_wallpaper ~= current_wallpaper then
+		current_wallpaper = new_wallpaper
+		window:set_config_overrides({
+			window_background_image = current_wallpaper,
+		})
+	end
+end
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -63,14 +77,24 @@ config.font = wezterm.font("0xProto Nerd Font", {
 })
 
 -- Set the initial random wallpaper
+current_wallpaper = get_random_wallpaper()
 config.window_background_opacity = 1
 config.text_background_opacity = 0.5
 config.window_background_image_hsb = {
 	hue = 1,
 	saturation = 0.7,
-	brightness = 0.1,
+	brightness = 0.05,
 }
-config.window_background_image = get_random_wallpaper()
+config.window_background_image = current_wallpaper
+
+-- Add keybinding to change wallpaper manually
+config.keys = {
+	{
+		key = "b",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action_callback(change_wallpaper),
+	},
+}
 
 -- Return the final configuration
 return config
